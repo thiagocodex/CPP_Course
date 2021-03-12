5237da5550e043e4db5ec69400379c3b657154bd
thiagocodex <thiago_codex@outlook.com.br>
2021-03-12 13:54:40 -0300

    Alterado, código c para c++ no arquivo Default_Locale.h

--- src/Aula 05 - Como o C++ funciona/Default_Locale.h
+++ src/Aula 05 - Como o C++ funciona/Default_Locale.h
@@ -1,4 +1,8 @@
 #pragma once
+#include <iostream>
+
+//Definição da localidade do sistema para o console
 static char* currentLocale = setlocale(LC_ALL, "");
 
-static int localeInfo = printf("%s\n\n", currentLocale);
+//Função para mostrar no console a localidade do sistema sempre que incluído este arquivo de cabeçalho
+std::ostream& print = std::cout << currentLocale << std::endl << std::endl;
